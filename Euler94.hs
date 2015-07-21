adbe1294f36e9bfaee0dc30b40819a8e24da6dde
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-20 20:32:44 -0700

    added comments

--- Euler94.hs
+++ Euler94.hs
@@ -1,15 +1,24 @@
 import           Control.Monad (replicateM)
 
+-- | Returns true if the the given double is integer valued.
 isInteger :: Double -> Bool
 isInteger x = x == fromInteger (round x)
 
+-- | Returns the area of the triangle formed by the given side lengths. Does not
+-- validate that the given lengths can actually form a triangle.
 areaT :: Double -> Double -> Double -> Double
 areaT x y z = sqrt $ p * (p - x) * (p - y) * (p - z)
   where p = (x + y + z) / 2
 
+-- | Returns a list of tuples of all almost equilateral triangles
+-- whose perimeters are less than @n@. The first element of the tuple is the
+-- length of the shorter sides, and the second element is the length of the
+-- longer side.
 allTriangles :: Integer -> [(Integer, Integer)]
 allTriangles n = [(x, y) | x <- [2..(n-1)], y <- [1..(n-1)], abs (x - y) == 1]
 
+-- | Returns the sum of the perimeters of all almost equilateral triangles with
+-- integral side lengths and area whose perimeters are less than @n@.
 euler94 :: Integer -> Integer
 euler94 n = sum $ do
   (x, y) <- allTriangles n
