472991fc18b9554ce966b1e4280cac3a61ddae4d
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-21 20:45:21 -0700

    cleaning up allTriangles

--- Euler94.hs
+++ Euler94.hs
@@ -1,7 +1,7 @@
 --{-# ANN allTriangles "HLint: ignore Redundant flip" #-}
 
 import           Control.Monad (replicateM)
-import           Data.List     (foldl')
+import           Data.List     (foldl', transpose)
 
 infixl 9 #
 (#) :: (a -> b) -> (b -> c) -> a -> c
@@ -21,17 +21,15 @@ areaT x y z = sqrt $ fromRational $ p * (p - x') * (p - y') * (p - z')
         y' = fromInteger y
         z' = fromInteger z
 
-merge :: [a] -> [a] -> [a]
-merge [] ys = ys
-merge (x:xs) ys = x : merge ys xs
-
 -- | Returns a list of tuples of all almost equilateral triangles with
 -- integer-valued sides and perimeter less than or equal to @n@. The first
 -- element of the tuple gives the length of either similar side, and the second
 -- element gives the length of the other side.
 allTriangles :: Integer -> [(Integer, Integer)]
 allTriangles n = takeWhile (\(x, y) -> 2 * x + y <= n) zs
-  where zs = (2, 1) : merge (zip [2..n] [3..n]) (zip [3..n] [2..n])
+  where zs = (2, 1) : (concat . transpose) [ zip [2..n] [3..n]
+                                           , zip [3..n] [2..n]
+                                           ]
 
 {-# ANN euler94 "HLint: ignore Redundant flip" #-}
 -- | Returns the sum of the perimeters of all almost equilateral triangles with
