dd85f5a575834cbe01ba1d4b8f57fa699617b987
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-21 18:31:58 -0700

    cleaned up allTriangles

--- Euler94.hs
+++ Euler94.hs
@@ -2,12 +2,6 @@ import           Control.Monad (replicateM)
 import           Data.List     (foldl')
 import           Data.Tuple    (swap)
 
-infixl 9 #
-(#) :: (a -> b) -> (b -> c) -> a -> c
--- ^ convenience infix op for reverse function composition
-(#) = flip (.)
-
-
 -- | Returns true if the the given double is integer valued.
 isInteger :: Double -> Bool
 isInteger x = x == fromInteger (round x)
@@ -21,26 +15,25 @@ areaT x y z = sqrt $ fromRational $ p * (p - x') * (p - y') * (p - z')
         y' = fromInteger y
         z' = fromInteger z
 
---duplicateEach = (>>= (replicate 2 >>= (\[x] -> [head x, swap $ last x])))
+merge :: [a] -> [a] -> [a]
+merge [] ys = ys
+merge (x:xs) ys = x : merge ys xs
 
 -- | Returns a list of tuples of all almost equilateral triangles with
 -- integer-valued sides and perimeter less than or equal to @n@. The first
--- element of the tuple is the length of two sides, and the second element is
--- the length of the other side.
+-- element of the tuple gives the length of either similar side, and the second
+-- element gives the length of the other side.
 allTriangles :: Integer -> [(Integer, Integer)]
-allTriangles n = do
-  let zs = zip [2..n] [1..n]
-      zs' = head zs : do z <- tail zs
-                         [swap z, z]
-  takeWhile (\(x, y) -> 2 * x + y <= n) zs'
+allTriangles n = takeWhile (\(x, y) -> 2 * x + y <= n) zs
+  where zs = (2, 1) : merge (zip [2..n] [3..n]) (zip [3..n] [2..n])
 
 -- | Returns the sum of the perimeters of all almost equilateral triangles with
 -- integral side lengths and area whose perimeters are less than or equal to
 -- @n@.
 euler94 :: Integer -> Integer
 euler94 n = foldl' (\acc (x, y) -> if isInteger $ areaT x x y
-                                  then acc + 2 * x + y
-                                  else acc) 0 $ allTriangles n
+                                   then acc + 2 * x + y
+                                   else acc) 0 $ allTriangles n
 
 main :: IO ()
 main = do
