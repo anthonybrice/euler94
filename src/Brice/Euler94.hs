8e4f0e68a8b2760eab9abdcb976909104bc11316
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-24 14:39:20 -0700

    moved to integral

--- src/Brice/Euler94.hs
+++ src/Brice/Euler94.hs
@@ -10,27 +10,27 @@ isInteger x = x == fromInteger (round x)
 
 -- | Returns the area of the triangle formed by the given side lengths. Does not
 -- validate that the given lengths can actually form a triangle.
-areaT :: Integer -> Integer -> Integer -> Double
+areaT :: Integral a => a -> a -> a -> Double
 areaT x y z = sqrt $ fromRational $ p * (p - x') * (p - y') * (p - z')
   where p  = (x' + y' + z') / 2 :: Rational
-        x' = fromInteger x
-        y' = fromInteger y
-        z' = fromInteger z
+        x' = fromIntegral x
+        y' = fromIntegral y
+        z' = fromIntegral z
 
 -- | Returns true if the given triangle has integer-valued area.
-intArea :: (Integer, Integer) -> Bool
+intArea :: Integral a => (a, a) -> Bool
 intArea (x, y) = isInteger $ areaT x x y
 
 -- | 'euler94' is the sum of the perimeters of all almost equilateral triangles
 -- with integral side lengths and area and whose perimeters do not exceed the
 -- argument.
-euler94 :: Integer -> Integer
+euler94 :: Integral a => a -> a
 euler94 = euler94' 16
 
 -- | Returns the sum of the perimeters of all almost equilateral triangles with
 -- integral side lengths and area between the triangles given by @m@ and @n@
 -- (inclusive).
-euler94' :: Integer -> Integer -> Integer
+euler94' :: Integral a => a -> a -> a
 euler94' m n = sum . map (\(x, y) -> 2 * x + y) . filter intArea
                $ allTriangles' m n
 
@@ -38,7 +38,7 @@ euler94' m n = sum . map (\(x, y) -> 2 * x + y) . filter intArea
 -- integer-valued sides between the triangles given by @m@ and @n@. The first
 -- element of the tuple gives the length of either similar side, and the second
 -- element gives the length of the other side.
-allTriangles' :: Integer -> Integer -> [(Integer, Integer)]
+allTriangles' :: Integral a => a -> a -> [(a, a)]
 allTriangles' m n = takeWhile (\(p, q) -> 2 * p + q <= n) zs
   where (x, y)   = triangle m
         (x', y') = nextT (x, y)
@@ -47,25 +47,24 @@ allTriangles' m n = takeWhile (\(p, q) -> 2 * p + q <= n) zs
                                         ]
 
 -- | Returns the almost equilateral triangle given by the perimeter @p@.
-triangle :: Integer -> (Integer, Integer)
+triangle :: Integral a => a -> (a, a)
 triangle p = if r == 1 then (q, q+1) else (q+1, q)
   where (q, r) = quotRem p 3
 
 -- | Returns the next almost equilateral triangle after the given one.
-nextT :: (Integer, Integer) -> (Integer, Integer)
+nextT :: Integral a => (a, a) -> (a, a)
 nextT (x, y) = if x < y then (y, x) else (x, y+2)
 
--- | A binary operator for the fold in 'main'.
-mainFold :: [(Integer, Integer)] -> Integer -> [(Integer, Integer)]
-mainFold [] n = init $ mainFold [(15, 0)] n
+mainFold :: Integral a => [(a, a)] -> a -> [(a, a)]
 mainFold acc n = (n, s + snd (head acc)) : acc
   where s = euler94' (fst (head acc) + 1) n
 
-mapNs :: [Integer] -> Map Integer Integer
-mapNs = fromList . foldl' mainFold [(15, 0)] . sort . nub
+-- | 'mapNs' is a map from the given list to their 'euler94' value.
+mapNs :: Integral a => [a] -> Map a a
+mapNs = fromList . init . foldl' mainFold [(15, 0)] . sort . nub
 
 main :: IO ()
 main = do
-  ns <- readLn >>= flip replicateM readLn
+  ns <- (readLn >>= flip replicateM readLn) :: IO [Integer]
   let vs = mapNs ns
   mapM_ (\n -> print $ vs!n) ns
