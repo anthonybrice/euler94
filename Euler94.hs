8947a4347728bee635f6462947ac0c15facacaf4
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-23 20:48:27 -0700

    cleaned up

--- Euler94.hs
+++ Euler94.hs
@@ -1,11 +1,5 @@
 import           Control.Monad (replicateM)
-import           Data.List     (elemIndex, foldl', nub, sort, transpose)
-import           Data.Maybe    (fromJust)
-
-infixl 9 #
-(#) :: (a -> b) -> (b -> c) -> a -> c
--- ^ convenience infix op for reverse function composition
-(#) = flip (.)
+import           Data.List     (transpose)
 
 -- | Returns true if the the given double is integer valued.
 isInteger :: Double -> Bool
@@ -33,42 +27,6 @@ allTriangles n = takeWhile (\(x, y) -> 2 * x + y <= n) zs
 -- | Returns the sum of the perimeters of all almost equilateral triangles with
 -- integral side lengths and area whose perimeters are less than or equal to
 -- @n@.
-euler94 :: Integer -> Integer
-euler94 = allTriangles
-          # flip foldl' 0 (\acc (x, y) -> if isInteger $ areaT x x y
-                                          then acc + 2 * x + y
-                                          else acc)
-
-euler94' :: (Integer, (Integer, Integer))
-         -> Integer
-         -> (Integer, (Integer, Integer))
-euler94' (p, t) n =
-  let (ts, nt) = allTriangles' t n
-      s = flip foldl' p (\acc (x, y) -> if isInteger $ areaT x x y
-                                        then acc + 2 * x + y
-                                        else acc) ts
-  in (s, nt)
-
-
-allTriangles' :: (Integer, Integer)
-              -> Integer
-              -> ([(Integer, Integer)], (Integer, Integer))
-allTriangles' t@(p, q) n =
-  let ts = takeWhile (\(x, y) -> 2 * x + y <= n) zs
-      (p', q') = nextT t
-      zs = (concat . transpose) [ zip [p..n] [q..n]
-                                , zip [p'..n] [q'..n]
-                                ]
-      nextT (y, z) = if y < z
-                     then (z, y)
-                     else (y, z+2)
-      t' = nextT $ last ts
-  in ts `seq` t' `seq` (ts, t')
-
-mainFold acc n =
-  let (s, nt) = euler94' (head acc) n
-  in s `seq` nt `seq` (s, nt) : acc
-
 euler94'' :: Integer -> Integer
 euler94'' n = sum $ map (\(x, y) -> x + x + y) $ filter intArea $ allTriangles n
 
@@ -87,9 +45,3 @@ triangle p =
 
 main :: IO ()
 main = readLn >>= flip replicateM readLn >>= mapM_ (print . euler94'')
-  -- ns <- readLn >>= flip replicateM readLn
-  -- let ns' = sort . nub $ ns
-  --     vs = reverse . init $! foldl' mainFold [(0, (2,1))] ns'
-  --     _ = map (`seq` undefined) vs
-  -- mapM_ (\n -> let i = fromJust $! elemIndex n ns'
-  --              in print $! fst $! vs!!i) ns
