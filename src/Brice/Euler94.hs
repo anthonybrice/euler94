2cebfdd4710f31b6cbded3df64ee8c04e65c4fc6
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-26 15:19:49 -0700

    optimizing

--- src/Brice/Euler94.hs
+++ src/Brice/Euler94.hs
@@ -1,6 +1,7 @@
 module Brice.Euler94 (euler94, euler94') where
 
-import           Data.List (transpose)
+import           Data.List                        (transpose)
+import           Math.NumberTheory.Powers.Squares (isSquare)
 
 -- | 'euler94' is the sum of the perimeters of all almost equilateral triangles
 -- with integral side lengths and area and whose perimeters do not exceed the
@@ -13,34 +14,35 @@ euler94 = euler94' 16
 -- (inclusive).
 euler94' :: Integral a => a -> a -> a
 euler94' m n = sum . map (\(x, y) -> 2 * x + y) . filter intArea
-               $ allTriangles' m n
+               $ allTriangles m n
 
 -- | Returns true if the given triangle has integer-valued area.
 intArea :: Integral a => (a, a) -> Bool
-intArea (x, y) = isInteger $ area x y
+intArea (x, y)
+  | odd y     = False
+  | otherwise = isSquare $ s * (s - x) * (s - x) * (s - y)
+  where s = (2 * x + y) `quot` 2
 
 -- | Returns true if the the given double is integer valued.
-isInteger :: Double-> Bool
+isInteger :: Double -> Bool
 isInteger x = x == fromInteger (round x)
 
 -- | Returns the area of the triangle formed by the given side lengths. Does not
 -- validate that the given lengths can actually form a triangle.
 area :: Integral a => a -> a -> Double
 area x y = y' * sqrt ((x' - y') * (x' + y'))
-  where
-    x' = fromIntegral x
-    y' = fromIntegral y / 2
-
+  where x' = fromIntegral x
+        y' = fromIntegral y / 2
 
 -- | Returns a list of tuples of all almost equilateral triangles with
 -- integer-valued sides between the triangles given by @m@ and @n@. The first
 -- element of the tuple gives the length of either similar side, and the second
 -- element gives the length of the other side.
-allTriangles' :: Integral a => a -> a -> [(a, a)]
-allTriangles' m n = takeWhile (\(p, q) -> 2 * p + q <= n) zs
+allTriangles :: Integral a => a -> a -> [(a, a)]
+allTriangles m n = takeWhile (\(p, q) -> 2 * p + q <= n) zs
   where (x, y)   = triangle m
         (x', y') = nextT (x, y)
-        zs       = (concat . transpose) [ zip [x..n] [y..n]
+        zs       = concat . transpose $ [ zip [x..n] [y..n]
                                         , zip [x'..n] [y'..n]
                                         ]
 
