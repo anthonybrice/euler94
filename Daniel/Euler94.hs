3e738aa8b448b3d1c5db287a4c8f121c672632a8
Daniel Brice <danielbrice@gmail.com>
2015-07-26 22:32:27 -0500

    included isPossibleSquare2 but it's not compiling

--- Daniel/Euler94.hs
+++ Daniel/Euler94.hs
@@ -1,5 +1,10 @@
 -- src/Briceest/Euler94.hs
+
+-- my imports
 import           Data.List (foldl')
+
+-- arithmoi imports below
+import Data.Array.Base (unsafeAt)
 
 isSquare :: Integer -> Bool
 isSquare = undefined
@@ -22,9 +27,38 @@ folder acc n =
                      else acc
                 1 -> acc
 
-euler94 :: (Integer, Integer) -- prior ending, prior sum
-        -> Integer            -- new ending
-        -> (Integer, Integer) -- new ending, new sum
-euler94 (pend, psum) nend =
-    (nend, foldl' folder psum [(pend + 1)..nend])
+euler94 :: (Integer, Integer) -- prior last perimeter, prior sum
+        -> Integer            -- new last perimeter
+        -> (Integer, Integer) -- new last perimeter, new sum
+euler94 (priorLast, priorSum) newLast =
+    (newLast, foldl' folder priorSum [(priorLast + 1)..newLast])
 -- invoke as `euler94 (4, 0) n` to run from 5 to n
+
+------------------------------------------------------------------------
+--  THERE BE DRAGONS HERE ----------------------------------------------
+
+-- | Test whether a non-negative number may be a square.
+--   Non-negativity is not checked, passing negative arguments may
+--   cause any kind of havoc.
+--
+--   First the remainder modulo 256 is checked (that can be calculated
+--   easily without division and eliminates about 82% of all numbers).
+--   After that, the remainders modulo several small primes are tested
+--   to eliminate altogether about 99.98954% of all numbers.
+--
+--   For smallish to medium sized numbers, this hardly performs better
+--   than 'isPossibleSquare', which uses smaller arrays, but for large
+--   numbers, where calculating the square root becomes more expensive,
+--   it is much faster (if the vast majority of tested numbers aren't squares).
+--{-# SPECIALISE isPossibleSquare2 :: Int -> Bool,
+--                                    Integer -> Bool,
+--                                    Word -> Bool
+--  #-}
+--isPossibleSquare2 :: Integral a => a -> Bool
+--isPossibleSquare2 n =
+--  unsafeAt sr256 ((fromIntegral n) .&. 255)
+--  && unsafeAt sr819  (fromIntegral (n `rem` 819))
+--  && unsafeAt sr1025 (fromIntegral (n `rem` 1025))
+--  && unsafeAt sr2047 (fromIntegral (n `rem` 2047))
+--  && unsafeAt sr4097 (fromIntegral (n `rem` 4097))
+--  && unsafeAt sr341  (fromIntegral (n `rem` 341))
